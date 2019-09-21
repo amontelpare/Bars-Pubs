0cdb27a83da9d610aeff2ead26404e09d3d2292b
Andrés Montelpare <andres.montelpare@etermax.com>
2019-09-21 17:06:52 -0300

    Initial Commit

---
+++ Bars&Pubs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bars&Pubs
+//
+//  Created by Andrés Montelpare on 21/09/2019.
+//  Copyright © 2019 Andrés Montelpare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bars&PubsTests/Bars_PubsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bars_PubsTests.swift
+//  Bars&PubsTests
+//
+//  Created by Andrés Montelpare on 21/09/2019.
+//  Copyright © 2019 Andrés Montelpare. All rights reserved.
+//
+
+import XCTest
+@testable import Bars_Pubs
+
+class Bars_PubsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
