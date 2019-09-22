798009af625185a40d2cd67ac065bd083c7d3849
Andrés Montelpare <andres.montelpare@etermax.com>
2019-09-22 18:20:17 -0300

    Create ListBarsAndPubsCell.

---
+++ Bars&Pubs/Scenes/ListBarsAndPubsScene/Cells/ListBarsAndPubsCell.swift
@@ -0,0 +1,21 @@
+//
+//  ListBarsAndPubsCell.swift
+//  Bars&Pubs
+//
+//  Created by Andrés Montelpare on 22/09/2019.
+//  Copyright © 2019 Andrés Montelpare. All rights reserved.
+//
+
+import UIKit
+
+class ListBarsAndPubsCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var thumbImageView: UIImageView!
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
