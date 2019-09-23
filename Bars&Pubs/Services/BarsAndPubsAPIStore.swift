0a956c75d5d2f5a1c90f239755540da63eb16ed1
Andrés Montelpare <andres.montelpare@etermax.com>
2019-09-22 23:28:16 -0300

    Handle http errors.

--- Bars&Pubs/Services/BarsAndPubsAPIStore.swift
+++ Bars&Pubs/Services/BarsAndPubsAPIStore.swift
@@ -16,30 +16,30 @@ class BarsAndPubsAPIStore: ListBarsAndPubsStoreProtocol {
     
     func fetchBarsAndPubs(category: Categories, sort: SortBy, order: SortOrder, count: Int, startAt: Int, completionHandler: @escaping (() throws -> SearchBarsAndPubsResponse) -> Void) {
         
-        let url = URL(string: "\(baseSearchUrl)start=\(startAt)&category=\(category.rawValue)&count=\(count)")!
+        let url = URL(string: "\(baseSearchUrl)start=\(startAt)&category=\(category.rawValue)&count=\(count)&sort=\(sort)&order=\(order)&entity_id=280&entity_type=city")!
         var request = URLRequest(url: url)
         request.addValue(ApiAuth.value.rawValue, forHTTPHeaderField: ApiAuth.key.rawValue)
         
         let task = session.dataTask(with: request, completionHandler: { data, response, error in
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                    completionHandler{ throw ListBarsAndPubsStoreError.ServerError }
+                    return
+            }
+
             if let data = data, error == nil {
                 do {
                     let decoder = JSONDecoder()
                     let searchBarsAndPubsResponse = try decoder.decode(SearchBarsAndPubsResponse.self, from: data)
                     completionHandler{ return searchBarsAndPubsResponse }
-                    
                 } catch let error {
-                    completionHandler{ throw ListBarsAndPubsStoreError.CannotFetch("") }
+                    completionHandler{ throw ListBarsAndPubsStoreError.DecodeError(error.localizedDescription) }
                 }
             } else {
-                completionHandler{ throw ListBarsAndPubsStoreError.CannotFetch("") }
+                completionHandler{ throw ListBarsAndPubsStoreError.CannotFetch(response!.url!.absoluteString) }
             }
         })
         task.resume()
     }
 }
 
-enum ApiAuth: String {
-    case key = "user-key"
-    case value = "afedf265c9253be3b47f0be8a4744c50"//"98946139a3526851e43b301a1832f3d7"
-}
-//afedf265c9253be3b47f0be8a4744c50
