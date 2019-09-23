5a3c833be550ac05da63d4c821be1724cae29a42
Andrés Montelpare <andres.montelpare@etermax.com>
2019-09-22 23:23:51 -0300

    Create RatingWorker tests.

---
+++ Bars&PubsTests/Workers/RatingWorkerTests.swift
@@ -0,0 +1,37 @@
+//
+//  RatingWorkerTests.swift
+//  Bars&PubsTests
+//
+//  Created by Andrés Montelpare on 22/09/2019.
+//  Copyright © 2019 Andrés Montelpare. All rights reserved.
+//
+
+@testable import Bars_Pubs
+import XCTest
+
+class RatingWorkerTests: XCTestCase {
+
+    func testRoundDown() {
+        let ratingWorker = RatingWorker()
+        let rating:CGFloat = 4.1
+        let roundedRating = ratingWorker.roundToDisplayStars(rating: rating)
+        
+        XCTAssertEqual(roundedRating, 4.0)
+    }
+    
+    func testRounpUp() {
+        let ratingWorker = RatingWorker()
+        let rating:CGFloat = 4.9
+        let roundedRating = ratingWorker.roundToDisplayStars(rating: rating)
+        
+        XCTAssertEqual(roundedRating, 5.0)
+    }
+
+    func testRounpMiddle() {
+        let ratingWorker = RatingWorker()
+        let rating:CGFloat = 4.6
+        let roundedRating = ratingWorker.roundToDisplayStars(rating: rating)
+        
+        XCTAssertEqual(roundedRating, 4.5)
+    }
+}
