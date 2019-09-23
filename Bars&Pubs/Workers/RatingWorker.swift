46def6c334fb6a0887bedec41c511feff3a4b5c7
Andrés Montelpare <andres.montelpare@etermax.com>
2019-09-22 23:23:26 -0300

    Create RatingWorker.

---
+++ Bars&Pubs/Workers/RatingWorker.swift
@@ -0,0 +1,19 @@
+//
+//  RatingWorker.swift
+//  Bars&Pubs
+//
+//  Created by Andrés Montelpare on 22/09/2019.
+//  Copyright (c) 2019 Andrés Montelpare. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class RatingWorker {
+    
+    func roundToDisplayStars(rating: CGFloat) -> CGFloat {
+        let roundedRating = (rating*2).rounded()/2.0
+        
+        return roundedRating
+    }
+}
